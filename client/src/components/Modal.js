c7b7d21f365e42ac602443a26242317c206b0b9b
Emilien <emilien.billy@gmail.com>
2022-08-18 16:09:37 +0200

    Installed react-hook-form for easier form validation

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,17 @@
 import { useContext, useState } from "react";
 import axios from "../api/axios";
 import { PartnersContext } from "../context/PartnersContext";
+import { useForm } from "react-hook-form";
 
 const Modal = ({ open, onClose }) => {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data);
+
     const { addPartners } = useContext(PartnersContext);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -20,8 +29,9 @@ const Modal = ({ open, onClose }) => {
                 active: active,
             });
             //Function created in the context to add the newly added partner into partners state
-            addPartners(response.data.data.job);
+            addPartners(response.data.data.partner[0]);
             console.log(response);
+            onClose();
         } catch (err) {}
     };
 
@@ -34,39 +44,16 @@ const Modal = ({ open, onClose }) => {
                     <div className="flex flex-col w-80 lg:w-96 justify-center bg-white py-4 px-8 rounded-md">
                         <span className="text-center mb-4 font-semibold">Ajouter un partenaire</span>
                         <div className="flex flex-col">
-                            <form className="flex flex-col text-md">
-                                <input
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    className="mb-4 p-2 bg-main-bg rounded"
-                                    type="text"
-                                    placeholder="Nom"
-                                />
-                                <input
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    className="mb-4 p-2 bg-main-bg rounded"
-                                    type="text"
-                                    placeholder="Email"
-                                />
-                                <input
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="mb-4 p-2 bg-main-bg rounded"
-                                    type="text"
-                                    placeholder="Mot de passe"
-                                />
-                                <select
-                                    value={active}
-                                    onChange={(e) => setActive(e.target.value)}
-                                    className="mb-4 p-2 bg-main-bg rounded"
-                                    name="status"
-                                    id="status-select">
+                            <form className="flex flex-col text-md" onSubmit={handleSubmit(onSubmit)}>
+                                <input className="mb-4 p-2 bg-main-bg rounded" type="text" placeholder="Nom" {...register("name")} />
+                                <input value={email} className="mb-4 p-2 bg-main-bg rounded" type="text" placeholder="Email" {...register("email")} />
+                                <input value={password} className="mb-4 p-2 bg-main-bg rounded" type="text" placeholder="Mot de passe" {...register("password")} />
+                                <select value={active} className="mb-4 p-2 bg-main-bg rounded" name="status" id="status-select" {...register("active")}>
                                     <option value="true">Actif</option>
                                     <option value="false">Inactif</option>
                                 </select>
                                 <div className="flex justify-center">
-                                    <button className="px-4 py-2 rounded bg-primary-button text-white" onClick={formSubmit} type="submit">
+                                    <button className="px-4 py-2 rounded bg-primary-button text-white" type="submit">
                                         Ajouter
                                     </button>
                                     <button className="px-4 ml-4 rounded bg-secondary-button text-white" onClick={onClose}>
